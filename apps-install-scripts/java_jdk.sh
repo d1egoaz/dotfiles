eea557053159509881a390545f4cd51a7257c577
Diego Alvarez <diego.alvarez.zuluaga@gmail.com>
2017-09-26 14:47:59 -0700

    cleanup

--- apps-install-scripts/java_jdk.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt-get update
-sudo apt-get install -y software-properties-common
-
-# Install java8
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-get update
-echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-sudo apt-get install -y oracle-java8-installer
