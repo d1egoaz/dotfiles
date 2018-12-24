9ce472c924c683a778474d735ca37176f01c1119
Diego Alvarez <diego.canada@icloud.com>
2018-12-23 17:27:02 -0800

    moved to gdrive

--- bin/rclone-local-to-remote.sh
+++ bin/rclone-local-to-remote.sh
@@ -2,7 +2,7 @@
 set -x
 
 if [ $# -eq 1 ]; then # when called with arguments it'll sync
-    rclone sync ~/onedrive/deft remote:deft --exclude ".git/**" -v
+    rclone sync ~/gdrive/deft remote:deft --exclude ".git/**" -v
 else
-    rclone sync ~/onedrive/deft remote:deft --exclude ".git/**" --dry-run -v
+    rclone sync ~/gdrive/deft remote:deft --exclude ".git/**" --dry-run -v
 fi
