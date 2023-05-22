f1ee5f4025345234ff0f31033ff83747826f6942
Diego Alvarez <diego.canada@icloud.com>
2023-05-22 15:28:25 -0700

    add a few additional programs

--- bootstrap-apps.sh
+++ bootstrap-apps.sh
@@ -1,7 +1,10 @@
-#!/bin/zsh
+#!/bin/sh
 
 brew install --cask alfred
 brew install --cask postman
 brew install --cask tidal
+brew install --cask iterm2
+brew install --cask kindle
 
-brew install emacs-plus@28 --with-no-frame-refocus --with-native-comp
+brew tap d12frosted/emacs-plus
+brew install emacs-plus@30 --with-no-frame-refocus --with-native-comp --with-savchenkovaleriy-big-sur-icon
