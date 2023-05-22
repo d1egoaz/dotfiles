d6c24dd329b991a45527a68c42221ce476531d22
Diego Alvarez <diego.canada@icloud.com>
2023-05-22 15:27:25 -0700

    remove oh-my-zsh and powerlevel10k
    testing starship
    
    and instead of oh-my-zsh plugins, I sourced directly the plugins that I needed

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 echo "Installing brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
 sleep 1
 echo "Success! Brew is installed."
 
 
-echo "Installing latest zsh and oh-my-zsh"
+echo "Installing latest zsh and friends"
 brew install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-brew install romkatv/powerlevel10k/powerlevel10k
+brew install starship
 sleep 1
 echo "Success! zsh and friends are installed."
 
@@ -19,13 +18,13 @@ echo "Success! zsh and friends are installed."
 echo "Installing other tools..."
 brew install git-crypt
 brew install gpg
+brew install pass
 brew install pinentry-mac
 brew install tree
 brew install stow
 
 echo "Installing fonts..."
 brew tap homebrew/cask-fonts
-brew install font-iosevka-ss08
 # TODO: install sf pro and sf mono from https://developer.apple.com/fonts/
 
 echo "Installing terminal helpers..."
@@ -36,9 +35,13 @@ brew install fzf
 brew install autojump
 brew install exa
 brew install bat
+brew install progress
 
 echo "Installing dev tools ..."
 brew install go
+go install golang.org/x/tools/cmd/goimports@latest
+go install golang.org/x/tools/gopls@latest
+brew install rust
 brew install jq
 brew install stern
 brew install ripgrep
@@ -47,9 +50,10 @@ brew install prettier
 brew install mermaid-cli
 brew install gum
 
+# k8s
+brew install kind
+
 echo "Install only tool that I'm going to use: Emacs ❤️ ..."
-brew tap d12frosted/emacs-plus
-brew update
 
 # for emacs
 brew install cmake
@@ -61,6 +65,7 @@ brew install pandoc
 brew install shellcheck
 brew install shfmt
 brew install universal-ctags
+brew install libvterm
 
 echo "Installing lsp servers ..."
 brew install yaml-language-server
