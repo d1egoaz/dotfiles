f039fef1daef97e1c920fdc4d8b8d17c6545a27e
Diego Alvarez <info@diegoa.ca>
2023-08-15 16:04:59 -0700

    add back docker

--- bootstrap-apps.sh
+++ bootstrap-apps.sh
@@ -6,6 +6,7 @@ brew install --cask tidal
 brew install --cask iterm2
 #brew install --cask kindle
 brew install --cask gpg-suite
+brew install --cask docker
 
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@30 --with-no-frame-refocus --with-native-comp --with-savchenkovaleriy-big-sur-icon --with-xwidgets
