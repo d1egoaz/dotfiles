86d0264e8f2e95b5ec71420a4fc3ccda67252b53
Diego Alvarez <info@diegoa.ca>
2023-08-15 16:02:46 -0700

    add gpg suite

--- bootstrap-apps.sh
+++ bootstrap-apps.sh
@@ -4,7 +4,8 @@ brew install --cask alfred
 brew install --cask postman
 brew install --cask tidal
 brew install --cask iterm2
-brew install --cask kindle
+#brew install --cask kindle
+brew install --cask gpg-suite
 
 brew tap d12frosted/emacs-plus
 brew install emacs-plus@30 --with-no-frame-refocus --with-native-comp --with-savchenkovaleriy-big-sur-icon --with-xwidgets
