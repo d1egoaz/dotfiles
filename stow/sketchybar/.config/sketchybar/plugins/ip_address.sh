3abbc97c2dc05d5307ca694bf1a7c224be927949
Diego Alvarez <info@diegoa.ca>
2025-05-12 21:13:44 -0700

    show wifi name instead of IP

--- stow/sketchybar/.config/sketchybar/plugins/ip_address.sh
+++ stow/sketchybar/.config/sketchybar/plugins/ip_address.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 
-#source "$HOME/.config/colors.sh" # Loads all defined colors
+get_wifi_ssid() {
+    for i in $(ifconfig -lX "en[0-9]"); do
+        SSID=$(ipconfig getsummary "$i" 2>/dev/null | awk '/ SSID/ {print $NF}')
+        [ -n "$SSID" ] && echo "$SSID" && return
+    done
+}
 
 IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
 IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
+WIFI_SSID=$(get_wifi_ssid)
 
 if [[ $IS_VPN != "" ]]; then
     # COLOR=$ACCENT_COLOR
     ICON=
-    LABEL="VPN"
+    LABEL="VPN/$WIFI_SSID"
 elif [[ $IP_ADDRESS != "" ]]; then
     COLOR=$BLUE
     ICON=
-    LABEL=$IP_ADDRESS
+    LABEL=$WIFI_SSID
 else
     COLOR=$RED
     ICON=
