d03d5247d5196ef80a33ce91d67d256cb7d29dbc
Diego Alvarez <info@diegoa.ca>
2025-05-14 16:35:06 -0700

    more networking info

--- stow/sketchybar/.config/sketchybar/plugins/ip_address.sh
+++ stow/sketchybar/.config/sketchybar/plugins/ip_address.sh
@@ -7,9 +7,15 @@ get_wifi_ssid() {
     done
 }
 
-IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1)
+IP_ADDRESS=$(scutil --nwi | grep address | sed 's/.*://' | tr -d ' ' | head -1 | awk -F. '{print $1"."$2"."$3".x"}')
 IS_VPN=$(scutil --nwi | grep -m1 'utun' | awk '{ print $1 }')
 WIFI_SSID=$(get_wifi_ssid)
+
+if [ -z "$WIFI_SSID" ]; then
+  WIFI_SSID="$IP_ADDRESS"
+  else
+  WIFI_SSID="$WIFI_SSID/$IP_ADDRESS"
+fi
 
 if [[ $IS_VPN != "" ]]; then
     # COLOR=$ACCENT_COLOR
