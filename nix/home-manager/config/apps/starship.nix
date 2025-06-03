4b76a5973979cef47b778327288cea9e32c54734
Diego Alvarez <info@diegoa.ca>
2025-06-03 12:10:19 -0700

    fmt

--- nix/home-manager/config/apps/starship.nix
+++ nix/home-manager/config/apps/starship.nix
@@ -95,7 +95,7 @@
       # =====================================================
 
       custom.direnv = {
-        detect_files = [".envrc"];
+        detect_files = [ ".envrc" ];
         format = "[ direnv](bold bright-yellow)";
         disabled = false;
       };
@@ -110,7 +110,7 @@
 
       custom.kubeinfo = {
         command = "sed -n 's/KUBE_CONTEXT=\\(.*\\)/Kube Context: \\1,/p; s/KUBE_NAMESPACE=\\(.*\\)/namespace: \\1/p' .kube-env | paste -sd ' ' -";
-        detect_files = [".kube-env"];
+        detect_files = [ ".kube-env" ];
         format = " [$output]($style)";
         disabled = false;
       };
@@ -123,75 +123,213 @@
       # Disabled modules #
       ####################
 
-      username = { disabled = true; };
-      hostname = { disabled = true; };
-      localip = { disabled = true; };
-      shlvl = { disabled = true; };
-      singularity = { disabled = true; };
-      kubernetes = { disabled = true; };
-      vcsh = { disabled = true; };
-      fossil_branch = { disabled = true; };
-      fossil_metrics = { disabled = true; };
-      git_commit = { disabled = true; };
-      git_metrics = { disabled = true; };
-      hg_branch = { disabled = true; };
-      pijul_channel = { disabled = true; };
-      docker_context = { disabled = true; };
-      package = { disabled = true; };
-      c = { disabled = true; };
-      cmake = { disabled = true; };
-      cobol = { disabled = true; };
-      daml = { disabled = true; };
-      dart = { disabled = true; };
-      deno = { disabled = true; };
-      dotnet = { disabled = true; };
-      elixir = { disabled = true; };
-      elm = { disabled = true; };
-      erlang = { disabled = true; };
-      fennel = { disabled = true; };
-      guix_shell = { disabled = true; };
-      haskell = { disabled = true; };
-      haxe = { disabled = true; };
-      helm = { disabled = true; };
-      java = { disabled = true; };
-      julia = { disabled = true; };
-      kotlin = { disabled = true; };
-      gradle = { disabled = true; };
-      lua = { disabled = true; };
-      nim = { disabled = true; };
-      ocaml = { disabled = true; };
-      opa = { disabled = true; };
-      perl = { disabled = true; };
-      php = { disabled = true; };
-      pulumi = { disabled = true; };
-      purescript = { disabled = true; };
-      raku = { disabled = true; };
-      rlang = { disabled = true; };
-      red = { disabled = true; };
-      scala = { disabled = true; };
-      solidity = { disabled = true; };
-      swift = { disabled = true; };
-      typst = { disabled = true; };
-      vlang = { disabled = true; };
-      vagrant = { disabled = true; };
-      zig = { disabled = true; };
-      buf = { disabled = true; };
-      conda = { disabled = true; };
-      meson = { disabled = true; };
-      spack = { disabled = true; };
-      memory_usage = { disabled = true; };
-      gcloud = { disabled = true; };
-      openstack = { disabled = true; };
-      azure = { disabled = true; };
-      env_var = { disabled = true; };
-      crystal = { disabled = true; };
-      sudo = { disabled = true; };
-      jobs = { disabled = true; };
-      battery = { disabled = true; };
-      status = { disabled = true; };
-      os = { disabled = true; };
-      container = { disabled = true; };
-      shell = { disabled = true; };
+      username = {
+        disabled = true;
+      };
+      hostname = {
+        disabled = true;
+      };
+      localip = {
+        disabled = true;
+      };
+      shlvl = {
+        disabled = true;
+      };
+      singularity = {
+        disabled = true;
+      };
+      kubernetes = {
+        disabled = true;
+      };
+      vcsh = {
+        disabled = true;
+      };
+      fossil_branch = {
+        disabled = true;
+      };
+      fossil_metrics = {
+        disabled = true;
+      };
+      git_commit = {
+        disabled = true;
+      };
+      git_metrics = {
+        disabled = true;
+      };
+      hg_branch = {
+        disabled = true;
+      };
+      pijul_channel = {
+        disabled = true;
+      };
+      docker_context = {
+        disabled = true;
+      };
+      package = {
+        disabled = true;
+      };
+      c = {
+        disabled = true;
+      };
+      cmake = {
+        disabled = true;
+      };
+      cobol = {
+        disabled = true;
+      };
+      daml = {
+        disabled = true;
+      };
+      dart = {
+        disabled = true;
+      };
+      deno = {
+        disabled = true;
+      };
+      dotnet = {
+        disabled = true;
+      };
+      elixir = {
+        disabled = true;
+      };
+      elm = {
+        disabled = true;
+      };
+      erlang = {
+        disabled = true;
+      };
+      fennel = {
+        disabled = true;
+      };
+      guix_shell = {
+        disabled = true;
+      };
+      haskell = {
+        disabled = true;
+      };
+      haxe = {
+        disabled = true;
+      };
+      helm = {
+        disabled = true;
+      };
+      java = {
+        disabled = true;
+      };
+      julia = {
+        disabled = true;
+      };
+      kotlin = {
+        disabled = true;
+      };
+      gradle = {
+        disabled = true;
+      };
+      lua = {
+        disabled = true;
+      };
+      nim = {
+        disabled = true;
+      };
+      ocaml = {
+        disabled = true;
+      };
+      opa = {
+        disabled = true;
+      };
+      perl = {
+        disabled = true;
+      };
+      php = {
+        disabled = true;
+      };
+      pulumi = {
+        disabled = true;
+      };
+      purescript = {
+        disabled = true;
+      };
+      raku = {
+        disabled = true;
+      };
+      rlang = {
+        disabled = true;
+      };
+      red = {
+        disabled = true;
+      };
+      scala = {
+        disabled = true;
+      };
+      solidity = {
+        disabled = true;
+      };
+      swift = {
+        disabled = true;
+      };
+      typst = {
+        disabled = true;
+      };
+      vlang = {
+        disabled = true;
+      };
+      vagrant = {
+        disabled = true;
+      };
+      zig = {
+        disabled = true;
+      };
+      buf = {
+        disabled = true;
+      };
+      conda = {
+        disabled = true;
+      };
+      meson = {
+        disabled = true;
+      };
+      spack = {
+        disabled = true;
+      };
+      memory_usage = {
+        disabled = true;
+      };
+      gcloud = {
+        disabled = true;
+      };
+      openstack = {
+        disabled = true;
+      };
+      azure = {
+        disabled = true;
+      };
+      env_var = {
+        disabled = true;
+      };
+      crystal = {
+        disabled = true;
+      };
+      sudo = {
+        disabled = true;
+      };
+      jobs = {
+        disabled = true;
+      };
+      battery = {
+        disabled = true;
+      };
+      status = {
+        disabled = true;
+      };
+      os = {
+        disabled = true;
+      };
+      container = {
+        disabled = true;
+      };
+      shell = {
+        disabled = true;
+      };
     };
   };
 }
