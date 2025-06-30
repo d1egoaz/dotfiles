b477e51d507dd9d3f2f4e4908847b723f952f1ae
Diego Alvarez <info@diegoa.ca>
2025-06-30 13:24:50 -0700

    update on click

--- nix/home-manager/config/apps/sketchybar/items/chym.sh
+++ nix/home-manager/config/apps/sketchybar/items/chym.sh
@@ -8,6 +8,7 @@ source "${CONFIG_DIR}/icons.sh"
 sketchybar --add item chym right \
   --set chym update_freq=300 \
   script="$CONFIG_DIR/plugins/chym.sh" \
+  click_script="$CONFIG_DIR/plugins/chym.sh" \
   label.color="$GREEN" \
   label.font="${FONT}:Regular:11.0" \
   icon=􀂘 \
